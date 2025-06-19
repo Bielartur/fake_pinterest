27893d21706479140f7b2cdf91e3a5a9e0e74001
Gabriel Canto <gabrielcanto2016@gmail.com>
2025-06-19 13:54:27 -0300

    Adicionando o login_required

--- fakepinterest/models.py
+++ fakepinterest/models.py
@@ -1,9 +1,14 @@
+from flask_login import UserMixin
+from sqlalchemy.testing.pickleable import User
+
+from fakepinterest import database, login_manager
 from datetime import datetime
 
-from fakepinterest import database
+@login_manager.user_loader
+def load_usuario(id_usuario):
+    return Usuario.query.get(int(id_usuario))
 
-
-class Usuario(database.Model):
+class Usuario(database.Model, UserMixin):
     id = database.Column(database.Integer, primary_key=True)
     username = database.Column(database.String(80), nullable=False)
     email = database.Column(database.String(255), nullable=False, unique=True)
