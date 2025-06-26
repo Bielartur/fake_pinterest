3b8245817a71753333a6743e6bf272340210a14e
Gabriel Canto <gabrielcanto2016@gmail.com>
2025-06-26 12:11:11 -0300

    Adicionando login atráves do usuário e tornando o username único

--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -5,12 +5,28 @@ from fakepinterest.models import Usuario
 
 
 class FormLogin(FlaskForm):
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
-    senha = PasswordField('Senha', validators=[DataRequired()])
+    login = StringField('E-mail ou nome de usuário', validators=[DataRequired()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(min=6)])
     botao_confirmacao = SubmitField('Fazer Login')
 
-    def validate_email(self, email):
-        usuario = Usuario.query.filter_by(email=email.data).first()
+    def validate_login(self, login):
+        """Aceita tanto e-mail quanto username:
+           1) se tiver '@' → valida formato com Email()
+           2) procura no banco por email OU username
+        """
+        texto = login.data.strip()
+        usuario = None
+        # 1) Se parece ser e-mail, valide formato RFC
+        if '@' in texto:
+            email_validator = Email('Email inválido')
+            email_validator(self, login)
+            # busca o cadastro do usuário pelo email
+            usuario = Usuario.query.filter_by(email=texto).first()
+
+        # caso ainda não tenha achado o usuário, busca pelo username
+        if not usuario:
+            usuario = Usuario.query.filter_by(username=texto).first()
+
         if not usuario:
             raise ValidationError('Usuário inexistente, crie uma conta')
 
@@ -26,6 +42,11 @@ class FormCriarConta(FlaskForm):
         usuario = Usuario.query.filter_by(email=email.data).first()
         if usuario:
             raise ValidationError('E-mail já cadastrado, faça login para continuar')
+
+    def validate_username(self, username):
+        usuario = Usuario.query.filter_by(username=username.data).first()
+        if usuario:
+            raise ValidationError('Nome de usuário já cadastrado, faça login para continuar')
 
 
 class FormFoto(FlaskForm):
