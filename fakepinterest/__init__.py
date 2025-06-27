9542d561799e76a5d8573fafb9e280319bf79354
Gabriel Canto <gabrielcanto2016@gmail.com>
2025-06-27 19:19:18 -0300

    Adicionando o front-end da função de deletar

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,12 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
 import os
 app = Flask(__name__)
+
+load_dotenv()
 if int(os.getenv('DEBUG')) == 0:
     LINK_BANCO = os.getenv('DATABASE_URL')
 else:
     LINK_BANCO = 'sqlite:///comunidade.db'
+
 app.config['SQLALCHEMY_DATABASE_URI'] = LINK_BANCO
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['UPLOAD_FOLDER'] = 'static/fotos_posts'
