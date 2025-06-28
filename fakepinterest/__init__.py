41f3dae318fd15d282dc216df0071cdd7cff7f97
Gabriel Canto <gabrielcanto2016@gmail.com>
2025-06-28 17:23:56 -0300

    Tentando melhorar a segurança do site para os navegadores

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,9 +2,43 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from flask_talisman import Talisman
 from dotenv import load_dotenv
 import os
 app = Flask(__name__)
+csp = {
+    'default-src': ["'self'"],
+
+    # libera CSS externo + inline
+    'style-src': [
+        "'self'",
+        "'unsafe-inline'",              # → autoriza style="…" e <style> inline
+        'https://fonts.googleapis.com', # Google Fonts
+        'https://cdnjs.cloudflare.com'  # Font Awesome CSS
+    ],
+
+    # libera fontes externas do Google e do Cloudflare (Font Awesome)
+    'font-src': [
+        "'self'",
+        'https://fonts.gstatic.com',    # Google Fonts
+        'https://cdnjs.cloudflare.com'  # Font Awesome woff/woff2
+    ],
+
+    'img-src': [
+        "'self'"
+    ],
+
+    'script-src': [
+        "'self'"
+    ]
+}
+
+# Aplica o Talisman com força de HTTPS e a CSP acima
+Talisman(
+    app,
+    force_https=True,
+    content_security_policy=csp
+)
 
 load_dotenv()
 if int(os.getenv('DEBUG')) == 0:
@@ -14,7 +48,6 @@ else:
 
 app.config['SQLALCHEMY_DATABASE_URI'] = LINK_BANCO
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-# app.config['UPLOAD_FOLDER'] = 'static/fotos_posts'
 app.config['UPLOAD_FOLDER_POSTS'] = 'static/fotos_posts'
 app.config['UPLOAD_FOLDER_PERFIL'] = 'static/fotos_perfil'
 
